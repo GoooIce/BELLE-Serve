b8e4eddfc6d27a448bb1f1e1855241690f36543e
王雪 <broken.ice1314@gmail.com>
2023-03-25 08:09:15 +0800

    device map auto

--- gen.py
+++ gen.py
@@ -91,6 +91,7 @@ class StreamModel:
                 else:
                     chunk_count = 0
                     yield self.tokenizer.decode(final_tokens.int(), skip_special_tokens=True)
+                    final_tokens = torch.tensor([], dtype=torch.int64)
 
             if chunk_count > 0:
                 yield self.tokenizer.decode(final_tokens.int(), skip_special_tokens=True)
--- model.py
+++ model.py
@@ -1,5 +1,8 @@
 from peft import PeftModel, get_peft_model
-from transformers import AutoTokenizer, AutoModelForCausalLM #, LlamaTokenizer, LlamaForCausalLM 
+# from transformers import LlamaTokenizer, LlamaForCausalLM 
+from transformers import AutoModelForCausalLM, AutoTokenizer, BitsAndBytesConfig
+
+
 
 def load_model(
     base="BelleGroup/BELLE-7B-2M",
@@ -16,10 +19,18 @@ def load_model(
     # )
     tokenizer =  AutoTokenizer.from_pretrained(base, add_eos_token=True)
 
+    # device_map = {
+    #     "transformer.word_embeddings": 0,
+    #     "transformer.word_embeddings_layernorm": 0,
+    #     "lm_head": "cpu",
+    #     "transformer.h": 0,
+    #     "transformer.ln_f": 0,
+    # }
+    quantization_config = BitsAndBytesConfig(llm_int8_enable_fp32_cpu_offload=True)
     model =  AutoModelForCausalLM.from_pretrained(
         base,
-        load_in_8bit=True,
         device_map="auto",
+        quantization_config=quantization_config,
     )
     
     # model = PeftModel.from_pretrained(model, finetuned, device_map={'': 0})
